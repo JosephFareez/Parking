4bed1627fc940caacfda0b1079f9aa31d20ef0dc
Joseph <joseph.fareez@gmail.com>
2025-06-04 21:31:52 +0300

    test

--- app/database.py
+++ app/database.py
@@ -1,3 +1,13 @@
+from typing import Annotated
+
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String
+from sqlalchemy.orm import mapped_column
+
+int_pk: type = Annotated[int, mapped_column(primary_key=True)]
+str10: type = Annotated[str, mapped_column(String(10), nullable=False)]
+str50: type = Annotated[str, mapped_column(String(50), nullable=False)]
+str100: type = Annotated[str, mapped_column(String(100), nullable=False)]
+
 
 db = SQLAlchemy()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from app import create_app
+from module_29_testing.hw.parking.app import create_app
 
 if __name__ == '__main__':
     app = create_app()
